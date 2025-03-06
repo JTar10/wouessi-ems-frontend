2ea7637209b5eb0768d4e146e2fa19f873d0b2e1
ouyix <xuouyi@gmail.com>
2025-03-06 12:49:30 -0500

    Created leave request popup

---
+++ src/pages/Leaves/Leaves.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import LeaveRequestPopup from "../../components//LeaveRequestPopup/LeaveRequestPopup.jsx";
+
+const Leaves = () => {
+  const [showPopup, setShowPopup] = useState(false);
+
+  const handleOpenPopup = () => {
+    setShowPopup(true);
+  };
+
+  const handleClosePopup = () => {
+    setShowPopup(false);
+  };
+
+  return (
+    <div className="leaves-page">
+      <h1>Leaves</h1>
+      <button onClick={handleOpenPopup}>Add leave request</button>
+      {showPopup && <LeaveRequestPopup onClose={handleClosePopup} />}
+    </div>
+  );
+};
+
+export default Leaves;
